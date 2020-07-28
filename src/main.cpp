8d8151e627e05bfba6da478fd964193bcc7ef793
MarkoGodra <marko.godra@gmail.com>
2020-07-28 20:06:04 +0200

    Naive lane following path generation

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -30,6 +31,11 @@ int main() {
 
   std::ifstream in_map_(map_file_.c_str(), std::ifstream::in);
 
+  if(in_map_.fail())
+  {
+      throw std::runtime_error("Failed to open map file");
+  }
+
   string line;
   while (getline(in_map_, line)) {
     std::istringstream iss(line);
@@ -50,8 +56,11 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
+  int lane = 1;
+  double ref_vel = 49.5;
+
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+               &map_waypoints_dx,&map_waypoints_dy, &ref_vel, &lane]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -88,17 +97,118 @@ int main() {
           //   of the road.
           auto sensor_fusion = j[1]["sensor_fusion"];
 
-          json msgJson;
+          vector<double> ptsx;
+          vector<double> ptsy;
 
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+
+          int prev_size = previous_path_x.size();
+
+          if(prev_size < 2)
+          {
+              // Initial case
+              double prev_car_x = car_x - cos(car_yaw);
+              double prev_car_y = car_y - sin(car_yaw);
+
+              // Use car current location and previous reconstructed from current yaw
+
+              ptsx.push_back(prev_car_x);
+              ptsx.push_back(car_x);
+              ptsy.push_back(prev_car_y);
+              ptsy.push_back(car_y);
+          }
+          else
+          {
+              // Normal case when we have previous
+              // Take two latest values
+              ref_x = previous_path_x[prev_size - 1];
+              ref_y = previous_path_y[prev_size - 1];
+
+              double ref_x_prev = previous_path_x[prev_size - 2];
+              double ref_y_prev = previous_path_y[prev_size - 2];
+              ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+              ptsx.push_back(ref_x_prev);
+              ptsx.push_back(ref_x);
+              ptsy.push_back(ref_y_prev);
+              ptsy.push_back(ref_y);
+          }
+
+
+          // Get anchors for spline
+          // 3 points 30 meters apart
+          for(auto i = 1; i <= 3; i++)
+          {
+              double s_increment = i * 30;
+              vector<double> xy = getXY(car_s + s_increment, (2 + 4 * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+              // Now we are generating path in front
+              ptsx.push_back(xy[0]);
+              ptsy.push_back(xy[1]);
+          }
+
+          // Transformation - Go from global coordiantes to car local frame
+          for(auto i = 0; i < ptsx.size(); i++)
+          {
+              double shift_x = ptsx[i] - ref_x;
+              double shift_y = ptsy[i] - ref_y;
+
+              ptsx[i] = (shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw));
+              ptsy[i] = (shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw));
+          }
+
+          // Create spline
+          tk::spline spline;
+
+          // Set anchor points for spline
+          spline.set_points(ptsx, ptsy);
+
+          // Now that we have spline all ready, we need to calculate filler points
           vector<double> next_x_vals;
           vector<double> next_y_vals;
 
-          /**
-           * TODO: define a path made up of (x,y) points that the car will visit
-           *   sequentially every .02 seconds
-           */
+          // Take uneaten points from previous path
+          for (auto i = 0; i < previous_path_x.size(); i++)
+          {
+              next_x_vals.push_back(previous_path_x[i]);
+              next_y_vals.push_back(previous_path_y[i]);
+          }
+
+          // Horizon
+          double target_x = 30.0;
+          double target_y = spline(target_x);
+          double target_dst = sqrt((target_x * target_x) + (target_y * target_y));
+
+          // Use this as our current position on x axis for generation of x axis values (increments)
+          double x_add_on = 0.0;
 
 
+          for(auto i = 1; i <= 50 - previous_path_x.size(); i++)
+          {
+            double N = target_dst / (0.02 * ref_vel / 2.24);
+            double x_point = x_add_on + (target_x / N);
+            double y_point = spline(x_point);
+
+            x_add_on = x_point;
+
+            double x_ref = x_point;
+            double y_ref = y_point;
+
+            // Transform back to global coordinates
+            x_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw));
+            y_point = (x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw));
+
+            x_point += ref_x;
+            y_point += ref_y;
+
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+          }
+
+          // Generate output message
+          json msgJson;
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
 
