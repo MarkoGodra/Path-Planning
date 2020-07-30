01447e3f00917576806c72dce3c1c79fd39b809e
MarkoGodra <marko.godra@gmail.com>
2020-07-30 21:01:56 +0200

    Refactor trajectory generator

--- src/trajectory_generator.cpp
+++ src/trajectory_generator.cpp
@@ -8,16 +8,23 @@ TrajectoryGenerator::TrajectoryGenerator(const Map& map) :
 
 Trajectory TrajectoryGenerator::GenerateTrajectory(const Vehicle& vehicle, int32_t target_lane, double target_vel)
 {
+    double ref_x = vehicle.car_x;
+    double ref_y = vehicle.car_y;
+    double ref_yaw = deg2rad(vehicle.car_yaw);
+    auto anchors = GenerateAnchorPoints(vehicle, kForwardAnchorsDistance, target_lane, ref_x, ref_y, ref_yaw);
+
+    return GenerateFillerPoints(vehicle, std::get<0>(anchors), std::get<1>(anchors), target_vel, ref_x, ref_y, ref_yaw);
+}
+
+std::tuple<std::vector<double>, std::vector<double>> TrajectoryGenerator::GenerateAnchorPoints(const Vehicle& vehicle, double forward_anchors_distance,
+                                                                                               int32_t target_lane, double& ref_x, double& ref_y, double& ref_yaw)
+{
     // Size of previous path
     int prev_size = vehicle.previous_path_x.size();
 
     // Path generation
     std::vector<double> ptsx;
     std::vector<double> ptsy;
-
-    double ref_x = vehicle.car_x;
-    double ref_y = vehicle.car_y;
-    double ref_yaw = deg2rad(vehicle.car_yaw);
 
     if(prev_size < 2)
     {
@@ -26,7 +33,6 @@ Trajectory TrajectoryGenerator::GenerateTrajectory(const Vehicle& vehicle, int32
         double prev_car_y = vehicle.car_y - sin(vehicle.car_yaw);
 
         // Use car current location and previous reconstructed from current yaw
-
         ptsx.push_back(prev_car_x);
         ptsx.push_back(vehicle.car_x);
         ptsy.push_back(prev_car_y);
@@ -52,9 +58,9 @@ Trajectory TrajectoryGenerator::GenerateTrajectory(const Vehicle& vehicle, int32
 
     // Get anchors for spline
     // 3 points 30 meters apart
-    for(auto i = 1; i <= 3; i++)
+    for(auto i = 1; i <= kNumOfAnchors; i++)
     {
-        double s_increment = i * 30;
+        double s_increment = i * forward_anchors_distance;
         vector<double> xy = getXY(vehicle.car_s + s_increment, (2 + 4 * target_lane), map_.map_waypoints_s, map_.map_waypoints_x, map_.map_waypoints_y);
 
         // Now we are generating path in front
@@ -62,21 +68,20 @@ Trajectory TrajectoryGenerator::GenerateTrajectory(const Vehicle& vehicle, int32
         ptsy.push_back(xy[1]);
     }
 
-    // Transformation - Go from global coordiantes to car local frame
-    for(auto i = 0; i < ptsx.size(); i++)
-    {
-        double shift_x = ptsx[i] - ref_x;
-        double shift_y = ptsy[i] - ref_y;
 
-        ptsx[i] = (shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw));
-        ptsy[i] = (shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw));
-    }
+    // Transform coordinate sys
+    GCoordSysToLocalCoord(ptsx, ptsy, ref_x, ref_y, ref_yaw);
 
+    return std::tie(ptsx, ptsy);
+}
+
+Trajectory TrajectoryGenerator::GenerateFillerPoints(const Vehicle& vehicle, const vector<double>& anchors_x, const vector<double>& anchors_y, const double& target_vel, const double& ref_x, const double& ref_y, const double& ref_yaw)
+{
     // Create spline
     tk::spline spline;
 
     // Set anchor points for spline
-    spline.set_points(ptsx, ptsy);
+    spline.set_points(anchors_x, anchors_y);
 
     // Now that we have spline all ready, we need to calculate filler points
     std::vector<double> next_x_vals;
@@ -90,13 +95,12 @@ Trajectory TrajectoryGenerator::GenerateTrajectory(const Vehicle& vehicle, int32
     }
 
     // Horizon
-    double target_x = 30.0;
+    double target_x = kHorizon;
     double target_y = spline(target_x);
     double target_dst = sqrt((target_x * target_x) + (target_y * target_y));
 
     // Use this as our current position on x axis for generation of x axis values (increments)
     double x_add_on = 0.0;
-
 
     for(auto i = 1; i <= 50 - vehicle.previous_path_x.size(); i++)
     {
@@ -121,4 +125,17 @@ Trajectory TrajectoryGenerator::GenerateTrajectory(const Vehicle& vehicle, int32
     }
 
     return Trajectory{next_x_vals, next_y_vals};
+}
+
+void TrajectoryGenerator::GCoordSysToLocalCoord(std::vector<double>& ptsx, std::vector<double>& ptsy, const double& ref_x, const double& ref_y, const double& ref_yaw)
+{
+    // Transformation - Go from global coordiantes to car local frame
+    for(auto i = 0; i < ptsx.size(); i++)
+    {
+        double shift_x = ptsx[i] - ref_x;
+        double shift_y = ptsy[i] - ref_y;
+
+        ptsx[i] = (shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw));
+        ptsy[i] = (shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw));
+    }
 }
--- src/trajectory_generator.h
+++ src/trajectory_generator.h
@@ -5,6 +5,7 @@
 #include "spline.h"
 #include "vehicle.h"
 
+#include <tuple>
 #include <vector>
 
 struct Trajectory
@@ -30,7 +31,21 @@ public:
     Trajectory GenerateTrajectory(const Vehicle& vehicle, int32_t target_lane, double target_vel);
 
 private:
+    std::tuple<std::vector<double>, std::vector<double>> GenerateAnchorPoints(const Vehicle& vehicle, double forward_anchors_distance,
+                                                                              int32_t target_lane, double& ref_x, double& ref_y, double& ref_yaw);
+    Trajectory GenerateFillerPoints(const Vehicle& vehicle,
+                                    const vector<double>& anchors_x,
+                                    const vector<double>& anchors_y,
+                                    const double& target_vel,
+                                    const double& ref_x,
+                                    const double& ref_y,
+                                    const double& ref_yaw);
+    void GCoordSysToLocalCoord(std::vector<double>& ptsx, std::vector<double>& ptsy,
+                               const double& ref_x, const double& ref_y, const double& ref_yaw);
     const Map map_;
+    static constexpr double kHorizon = 30.0;
+    static constexpr int32_t kNumOfAnchors = 3;
+    static constexpr double kForwardAnchorsDistance = 30.0;
 };
 
 #endif // TRAJECTORY_GENERATOR_H
