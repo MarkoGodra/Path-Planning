d5de2f3a7a0dbd98152cbd745d7bed8224de70da
MarkoGodra <marko.godra@gmail.com>
2020-07-31 12:34:18 +0200

    Initial state machine impl

--- src/vehicle_controller.cpp
+++ src/vehicle_controller.cpp
@@ -3,10 +3,11 @@
 #include <iostream>
 
 VehicleController::VehicleController(const Map& map) :
-    state_(VehicleState::INIT),
+    state_(VehicleState::KL),
     lane_(1.0),
     ref_vel_(0.0),
-    trajectory_generator_(map)
+    trajectory_generator_(map),
+    lanes_occupancy_()
 {
 
 }
@@ -24,43 +25,51 @@ Trajectory VehicleController::UpdatePath(Vehicle& vehicle,
         vehicle.car_s = vehicle.end_path_s;
     }
 
-    // There is no collision warning on each start of each iteration
-    bool current_lane_occupied;
-    bool left_lane_occupied;
-    bool right_lane_occupied;
-    double forward_vehicle_speed = -1.0;
-    std::tuple<bool, bool, bool> lanes_occupancy = CheckSurroundings(vehicle, predictions_begin, predictions_end);
-    SelectManeuver(vehicle, predictions_begin, predictions_end, lanes_occupancy);
+    CheckSurroundings(vehicle, predictions_begin, predictions_end);
+    SelectManeuver(vehicle);
 
     return trajectory_generator_.GenerateTrajectory(vehicle, lane_, ref_vel_);
 }
 
-std::tuple<bool, bool, bool> VehicleController::CheckSurroundings(Vehicle& vehicle, json::iterator& predictions_begin, json::iterator predictions_end)
+void VehicleController::CheckSurroundings(Vehicle& vehicle, json::iterator& predictions_begin, json::iterator predictions_end)
 {
-    // There is no collision warning on each start of each iteration
-    bool current_lane_occupied = false;
-    bool left_lane_occupied = false;
-    bool right_lane_occupied = false;
+    for(auto i = 0; i < kNumOfLanes; i++)
+    {
+        lanes_occupancy_[i].clear();
+    }
 
     // Check detections for car in our lane
     for(auto& it = predictions_begin; it != predictions_end; it++)
     {
-        // Detected car d coordinate
-        double d = (*it)[6];
+        DetectedVehicle detected_vehicle{
+            (*it)[0],
+                    (*it)[1],
+                    (*it)[2],
+                    (*it)[3],
+                    (*it)[4],
+                    (*it)[5],
+                    (*it)[6],
+        };
+
+        // Check if detection is in region of interest
+        if(!((detected_vehicle.s < vehicle.car_s + kBufferDistance) && (detected_vehicle.s > vehicle.car_s - kBufferDistance)))
+        {
+            continue;
+        }
 
         // Find out detected car lane
-        int car_lane = 0;
-        if(d >= 0 && d < kLaneWidth)
+        int detected_car_lane = 0;
+        if(detected_vehicle.d >= 0 && detected_vehicle.d < kLaneWidth)
         {
-            car_lane = 0;
+            detected_car_lane = 0;
         }
-        else if(d >= kLaneWidth && d < 2 * kLaneWidth)
+        else if(detected_vehicle.d >= kLaneWidth && detected_vehicle.d < 2 * kLaneWidth)
         {
-            car_lane = 1;
+            detected_car_lane = 1;
         }
-        else if(d >= 2 * kLaneWidth && d <= 3 * kLaneWidth)
+        else if(detected_vehicle.d >= 2 * kLaneWidth && detected_vehicle.d <= 3 * kLaneWidth)
         {
-            car_lane = 2;
+            detected_car_lane = 2;
         }
         else
         {
@@ -68,65 +77,161 @@ std::tuple<bool, bool, bool> VehicleController::CheckSurroundings(Vehicle& vehic
             continue;
         }
 
-        double vx = (*it)[3];
-        double vy = (*it)[4];
-
-        double check_car_speed = sqrt(vx * vx + vy * vy);
-        double check_car_s = (*it)[5];
-
-        check_car_s += static_cast<double>(vehicle.previous_path_x.size()) * 0.02 * check_car_speed;
-
-        // detection in front
-        if(car_lane == lane_)
-        {
-            current_lane_occupied |= (check_car_s > vehicle.car_s) && ((check_car_s - vehicle.car_s) <= kBufferDistance);
-        }
-        else if(lane_ == car_lane - 1)
-        {
-            left_lane_occupied |= (check_car_s <= (vehicle.car_s + kBufferDistance)) && (check_car_s >= (vehicle.car_s - kBufferDistance));
-        }
-        else if(lane_ == car_lane + 1)
-        {
-            right_lane_occupied |= (check_car_s <= (vehicle.car_s + kBufferDistance)) && (check_car_s >= (vehicle.car_s - kBufferDistance));
-        }
+        lanes_occupancy_[detected_car_lane].push_back(detected_vehicle);
     }
-
-    return std::tie(current_lane_occupied, left_lane_occupied, right_lane_occupied);
 }
 
-void VehicleController::SelectManeuver(const Vehicle& vehicle,
-                                       json::iterator& predictions_begin,
-                                       json::iterator predictions_end,
-                                       const std::tuple<bool, bool, bool>& lanes_occupancy)
+void VehicleController::SelectManeuver(const Vehicle& vehicle)
 {
-    bool current_lane_occupied = std::get<0>(lanes_occupancy);
-    bool left_lane_occupied = std::get<1>(lanes_occupancy);
-    bool right_lane_occupied = std::get<2>(lanes_occupancy);
+    switch (state_) {
+    case VehicleState::KL:
+        // Keep lane state
+        HandleKLState(vehicle);
+        break;
 
-    if(current_lane_occupied)
+    case VehicleState::LCL:
+        // LCL state
+        HandleLCLState(vehicle);
+        break;
+
+    case VehicleState::LCR:
+        // LCR state
+        HandleLCRState(vehicle);
+
+        break;
+    }
+}
+
+bool VehicleController::CurrentLaneOccuied(const Vehicle& vehicle)
+{
+    if(lanes_occupancy_[lane_].size() == 0)
     {
-        // If left lane change is possible
-        if(!left_lane_occupied && lane_ > 0)
-        {
-            lane_--;
-        }
-        // If right lane change is possible
-        else if(!right_lane_occupied && lane_ < 2)
-        {
-            lane_++;
-        }
-        // If both conditions fail keep lane and slow down
-        else
-        {
-            ref_vel_ -= 0.224;
-        }
+        return false;
     }
     else
     {
+        bool current_lane_occupied = false;
+        for(const auto& detection : lanes_occupancy_[lane_])
+        {
+            double vx = detection.x_vel;
+            double vy = detection.y_vel;
+
+            double check_car_speed = sqrt(vx * vx + vy * vy);
+            double check_car_s = detection.s;
+
+            check_car_s += 0.02 * check_car_speed;
+
+            // detection in front
+            current_lane_occupied |= (check_car_s > vehicle.car_s) && ((check_car_s - vehicle.car_s) <= kBufferDistance);
+        }
+
+        return current_lane_occupied;
+    }
+}
+
+void VehicleController::HandleKLState(const Vehicle& vehicle)
+{
+    bool current_lane_occupied = CurrentLaneOccuied(vehicle);
+
+    if(current_lane_occupied)
+    {
+        std::cout << "Occupied" << std::endl;
+        ref_vel_ -= 0.224;
+    }
+    else
+    {
+        // If lane is not occupied, stay in the same lane and don't accelerate if possible
         // Lane keeping, drive the speed limit
         if(ref_vel_ < kSpeedLimit)
         {
             ref_vel_ += 0.224;
         }
+    }
+
+    // TODO: Analyze potential overtakes
+    if(current_lane_occupied)
+    {
+        bool left_turn_possible = false;
+        bool right_turn_possible = false;
+
+        if(lane_ > 0)
+        {
+            left_turn_possible = true;
+        }
+
+        if(lane_ < 2)
+        {
+            right_turn_possible = true;
+        }
+
+        if(left_turn_possible)
+        {
+            int32_t potential_lane = lane_ - 1;
+
+            // If left lane is unoccupied
+            if(lanes_occupancy_[potential_lane].size() == 0)
+            {
+                // State transition to LCL
+                std::cout << "Transition to LCL" << std::endl;
+                lane_--;
+                state_ = VehicleState::LCL;
+            }
+        }
+        else if(right_turn_possible)
+        {
+            int32_t potential_lane = lane_ + 1;
+
+            // If left lane is unoccupied
+            if(lanes_occupancy_[potential_lane].size() == 0)
+            {
+                // State transition to LCL
+                std::cout << "Transition to LCL" << std::endl;
+                lane_++;
+                state_ = VehicleState::LCR;
+            }
+        }
+    }
+}
+
+void VehicleController::HandleLCLState(const Vehicle& vehicle)
+{
+    // TODO: Check for front collision and slow down if needed
+
+    if(IsLaneChangedCompleted(vehicle))
+    {
+        std::cout << "Transition to KL" << std::endl;
+        state_ = VehicleState::KL;
+    }
+    else
+    {
+        std::cout << "Changing lane" << std::endl;
+    }
+}
+
+void VehicleController::HandleLCRState(const Vehicle& vehicle)
+{
+    // TODO: Check for front collision and slow down if needed
+
+    if(IsLaneChangedCompleted(vehicle))
+    {
+        std::cout << "Transition to KL" << std::endl;
+        state_ = VehicleState::KL;
+    }
+    else
+    {
+        std::cout << "Changing lane" << std::endl;
+    }
+}
+
+bool VehicleController::IsLaneChangedCompleted(const Vehicle& vehicle)
+{
+    double target_lane_center = lane_ * kLaneWidth + (kLaneWidth / 2);
+    if((vehicle.car_d >= target_lane_center - kLaneWidth / 5.0) && (vehicle.car_d <= target_lane_center + kLaneWidth / 5.0))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
     }
 }
--- src/vehicle_controller.h
+++ src/vehicle_controller.h
@@ -10,10 +10,20 @@
 
 enum class VehicleState : uint8_t
 {
-    INIT,
     KL,
     LCL,
     LCR
+};
+
+struct DetectedVehicle
+{
+    uint32_t id;
+    double x;
+    double y;
+    double x_vel;
+    double y_vel;
+    double s;
+    double d;
 };
 
 class VehicleController
@@ -25,18 +35,27 @@ public:
     Trajectory UpdatePath(Vehicle& vehicle, json::iterator& predictions_begin, json::iterator predictions_end);
 
 private:
-    void SelectManeuver(const Vehicle& vehicle, json::iterator& predictions_begin,
-                        json::iterator predictions_end, const std::tuple<bool, bool, bool>& lanes_occupancy);
-    std::tuple<bool, bool, bool> CheckSurroundings(Vehicle& vehicle, json::iterator& predictions_begin, json::iterator predictions_end);
+    static constexpr int32_t kNumOfLanes = 3;
+    static constexpr double kSpeedLimit = 49.5;
+    static constexpr double kBufferDistance = 30.0;
+    static constexpr double kLaneWidth = 4.0;
+
+    void SelectManeuver(const Vehicle& vehicle);
+    void CheckSurroundings(Vehicle& vehicle, json::iterator& predictions_begin, json::iterator predictions_end);
+    bool CurrentLaneOccuied(const Vehicle& vehicle);
+    bool IsLaneChangedCompleted(const Vehicle& vehicle);
+
+    // State specific functions
+    void HandleKLState(const Vehicle& vehicle);
+    void HandleLCLState(const Vehicle& vehicle);
+    void HandleLCRState(const Vehicle& vehicle);
 
     VehicleState state_;
     int32_t lane_;
     double ref_vel_;
     TrajectoryGenerator trajectory_generator_;
 
-    static constexpr double kSpeedLimit = 49.5;
-    static constexpr double kBufferDistance = 30.0;
-    static constexpr double kLaneWidth = 4.0;
+    std::array<std::vector<DetectedVehicle>, kNumOfLanes> lanes_occupancy_;
 };
 
 #endif // VEHICLE_CONTROLLER_H
